239a67134dc8c607cb0220872c462f9e55643284
eechavarriag <eugenia.echavarria@richmond.edu>
2026-07-10 09:48:58 -0400

    Fix image path in config.js
    Corrected the image path for the Nepal earthquake analysis project.

--- config.js
+++ config.js
@@ -292,7 +292,7 @@ const CONFIG = {
     },
    
      {
-  image:       "images/projects/nepal-earthquake-analysis.jpg",
+  image:       "iimages/projects/nepal-earthquake-analysis.jpg",
   title:       "Impacted Population in Nepal from 2014 Earthquakes",
   course:      "GEOG 365: Advanced Spatial Analysis",
   date:        "Spring 2025",
